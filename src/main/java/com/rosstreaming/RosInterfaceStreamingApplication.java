c4daeeb9e6216df98c313c4b822c6e2211db538e
bdvalero98 <danilovalero25@gmail.com>
2025-05-27 19:39:28 -0500

    Initial commit: Spring Boot base project for Mikrotik streaming

---
+++ src/main/java/com/rosstreaming/RosInterfaceStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.rosstreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosInterfaceStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosInterfaceStreamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rosstreaming/RosInterfaceStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosstreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosInterfaceStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
